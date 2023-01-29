cb38fbefb29f87d639b48b6dbb4e5d5e9fbd65f9
Yash <yash191174@gmail.com>
2023-01-30 01:48:58 +0530

    new

--- EcommerceWeb/settings.py
+++ EcommerceWeb/settings.py
@@ -134,5 +134,5 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
