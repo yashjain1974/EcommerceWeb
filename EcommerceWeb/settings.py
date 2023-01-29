2052b2bb075275a74e35812170ddb25e7513844b
Yash <yash191174@gmail.com>
2023-01-30 01:45:33 +0530

    Added new files

--- EcommerceWeb/settings.py
+++ EcommerceWeb/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-huqi#v(u9ho)&$p*^)b#b7(xa*%k@%(kv&zj(8rh%$j$vj4i&$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+
 
 
 # Application definition
@@ -131,3 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Managing media
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
